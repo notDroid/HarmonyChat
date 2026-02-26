730e2fd91ca2c2a6cb4e1349fe504fb2bd613bb8
Wali Mirza <walimirza9982@gmail.com>
2026-02-26 07:46:44 -0500

    Improved loading/rendering logic for chat panel. Chat header shows up instantly

--- frontend/src/app/(main)/chats/[chat_id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingChatWindowView from "@/features/chat/ui/loading_screen"
-
-export default async function LoadingChatWindow() {
-    return <LoadingChatWindowView />;
-}
--- frontend/src/features/chat/view/chat.tsx
+++ frontend/src/features/chat/view/chat.tsx
@@ -1,18 +1,20 @@
+import { Suspense } from "react";
+
 // Views
 import ChatHeaderComponent from "../components/header";
 import ChatPanel from "../components/panel";
 
 // UI Components
 import ChatBar from "../components/bar";
-
-// API Functions
 import { NetworkError } from "@/lib/utils/errors";
+import LoadingChatPanel from "../ui/loading";
 
 // React Query
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { prefetchChatHistory } from "../api/cache";
 
-export default async function ChatWindowView({ chat_id }: { chat_id: string }) {
+// Try to prefetch the chat history on the server to populate the cache before rendering the ChatPanel
+async function PrefetchedChatPanel({ chat_id }: { chat_id: string }) {
   const queryClient = new QueryClient();
 
   try {
@@ -21,15 +23,25 @@ export default async function ChatWindowView({ chat_id }: { chat_id: string }) {
     if (!(error instanceof NetworkError)) {
       throw error;
     }
-    // Gracefuly fail to load on network error and let client handle it
   }
 
   return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ChatPanel chat_id={chat_id} />
+    </HydrationBoundary>
+  );
+}
+
+export default function ChatWindowView({ chat_id }: { chat_id: string }) {
+  return (
     <div className="flex h-full w-full flex-col min-w-0">
       <ChatHeaderComponent chat_id={chat_id} />
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <ChatPanel chat_id={chat_id} />
-        </HydrationBoundary>
+      
+      {/* Wait for the server to get this component only, everything else can be loaded on the client side immediately */ }
+      <Suspense fallback={<LoadingChatPanel />}>
+        <PrefetchedChatPanel chat_id={chat_id} />
+      </Suspense>
+      
       <ChatBar chat_id={chat_id} />
     </div>
   );
