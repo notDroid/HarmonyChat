c5d0bfbc75e89b785470095646249a0199aa439f
Wali Mirza <walimirza9982@gmail.com>
2026-03-29 22:47:32 -0400

    updated terraform bootstrap command to be dependent on aws account

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "harmony-chat-tf-state"
+    # bucket         = "harmony-chat-tf-state"
     key            = "bootstrap/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "harmony-chat-tf-locks"
+    # dynamodb_table = "harmony-chat-tf-locks"
   }
   
   required_providers {
@@ -29,11 +29,20 @@ provider "aws" {
   }
 }
 
+# Fetch the current AWS Account ID dynamically
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id  = data.aws_caller_identity.current.account_id
+  bucket_name = "harmony-chat-tf-state-${local.account_id}"
+  table_name  = "harmony-chat-tf-locks-${local.account_id}"
+}
+
 # ------------------------------------------------------------------------------
 # S3 Bucket for Terraform State
 # ------------------------------------------------------------------------------
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "harmony-chat-tf-state"
+  bucket = local.bucket_name
 
   lifecycle {
     prevent_destroy = true
@@ -72,7 +81,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_access" {
 # DynamoDB Table for State Locking
 # ------------------------------------------------------------------------------
 resource "aws_dynamodb_table" "terraform_state_locks" {
-  name         = "harmony-chat-tf-locks"
+  name         = local.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
--- infra/terraform/root.hcl
+++ infra/terraform/root.hcl
@@ -1,3 +1,7 @@
+locals {
+  account_id = get_aws_account_id()
+}
+
 generate "provider" {
   path      = "provider.tf"
   if_exists = "overwrite_terragrunt"
@@ -21,10 +25,10 @@ remote_state {
     if_exists = "overwrite_terragrunt"
   }
   config = {
-    bucket         = "harmony-chat-tf-state"
+    bucket         = "harmony-chat-tf-state-${local.account_id}"
     key            = "${path_relative_to_include()}/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "harmony-chat-tf-locks"
+    dynamodb_table = "harmony-chat-tf-locks-${local.account_id}"
   }
 }
